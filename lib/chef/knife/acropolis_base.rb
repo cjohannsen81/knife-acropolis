cbf9e1da30ab21ef35f9780a2f918dde3256c917
Christian Johannsen <c.johannsen@clickedways.de>
2016-06-16 09:03:01 +0200

    added ruby changes

--- lib/chef/knife/acropolis_base.rb
+++ lib/chef/knife/acropolis_base.rb
@@ -3,28 +3,29 @@ require 'digest'
 
 class Chef
   class Knife
+    #Base module for Acropolis
     module AcropolisBase
-    
-    def self.included(includer)
-      includer.class_eval do
-    
+
+    def self.included(i)
+      i.class_eval do
+
         deps do
           require 'chef/knife'
           require 'rest-client'
         end
-      
+
         option :a_host,
         :short => "-H HOST",
         :long => "--acropolis-host HOST",
         :description => "Please enter the IP/hostname of the Nutanix Management API host.",
         :proc => Proc.new { |key| Chef::Config[:knife][:a_host] = key }
-    
+
         option :a_user,
         :short => "-U USER",
         :long => "--acropolis-user USER",
         :description => "Please enter the IP/hostname of the Nutanix Management API host.",
         :proc => Proc.new { |key| Chef::Config[:knife][:a_user] = key }
-    
+
         option :a_pass,
         :short => "-P PASS",
         :long => "--acropolis-password PASS",
@@ -32,7 +33,7 @@ class Chef
         :proc => Proc.new { |key| Chef::Config[:knife][:a_pass] = key }
     end
   end
-  
+
   def validate
     unless Chef::Config[:knife][:a_user] && Chef::Config[:knife][:a_pass] && Chef::Config[:knife][:a_host]
       ui.error("Missing Credentials and/or host. Please use --help to see all available options.")
@@ -43,53 +44,53 @@ class Chef
   def get(url)
     validate
       base_url = "https://"+ Chef::Config[:knife][:a_host] +":9440/api/nutanix/v0.8"+url
-      puts "Trying to connect Acropolis on "+base_url 
+      puts "Trying to connect Acropolis on "+base_url
       RestClient::Request.execute(
-        :method => :get, 
-        :url => base_url, 
-        :content_type => 'json', 
-        :accept => 'json', 
-        :user => Chef::Config[:knife][:a_user], 
-        :password => Chef::Config[:knife][:a_pass], 
+        :method => :get,
+        :url => base_url,
+        :content_type => 'json',
+        :accept => 'json',
+        :user => Chef::Config[:knife][:a_user],
+        :password => Chef::Config[:knife][:a_pass],
         :verify_ssl => false
-      ) 
+      )
   end
-  
+
   def post(url,data)
     validate
       base_url = "https://"+ Chef::Config[:knife][:a_host] +":9440/api/nutanix/v0.8"+url
-      puts "Trying to connect Acropolis on "+base_url 
+      puts "Trying to connect Acropolis on "+base_url
       RestClient::Request.execute(
-        :method => :post, 
-        :url => base_url, 
+        :method => :post,
+        :url => base_url,
         :payload => data,
         #Please note the change in the headers, POST will not work with other options (Error 512).
         :headers => {:accept => :json},
         :headers => {:content_type => :json},
-        :user => Chef::Config[:knife][:a_user], 
-        :password => Chef::Config[:knife][:a_pass], 
+        :user => Chef::Config[:knife][:a_user],
+        :password => Chef::Config[:knife][:a_pass],
         :verify_ssl => false
-        ) 
+        )
     end
-    
+
     def delete(url,data)
     validate
         base_url = "https://"+ Chef::Config[:knife][:a_host] +":9440/api/nutanix/v0.8"+url
-        puts "Trying to connect Acropolis on "+base_url 
+        puts "Trying to connect Acropolis on "+base_url
         RestClient::Request.execute(
-          :method => :delete, 
+          :method => :delete,
           :url => base_url + "/" + data,
           #Please note the change in the headers, POST will not work with other options (Error 512).
           :headers => {:accept => :json},
           :headers => {:content_type => :json},
-          :user => Chef::Config[:knife][:a_user], 
-          :password => Chef::Config[:knife][:a_pass], 
+          :user => Chef::Config[:knife][:a_user],
+          :password => Chef::Config[:knife][:a_pass],
           :verify_ssl => false
-          ) 
+          )
       end
-    
+
   end
-  
-  
+
+
  end
 end
